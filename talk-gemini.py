e244c4d96d2adc5ff1ece52cb38693da82621190
Lee, Jun Seok <lego3410@gmail.com>
2024-10-14 01:55:44 +0900

    Add `talk-gemini.py`

---
+++ talk-gemini.py
@@ -0,0 +1,20 @@
+import os
+
+import google.generativeai as genai
+
+GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+chat = model.start_chat()
+
+
+def speak(text):
+  response = chat.send_message(text)
+  return response.text
+
+
+while (prompt := input("You: ")) != "":
+  print("Bot:", speak(prompt))
