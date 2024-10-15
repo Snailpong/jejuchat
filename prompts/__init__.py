254a3ef066456a3a910956f262bf7a61d50ced91
sp-v6x <seongsu.park@voyagerx.com>
2024-10-15 18:47:02 +0900

    Add cannot_generate_sql prompt

--- prompts/__init__.py
+++ prompts/__init__.py
@@ -1,3 +1,4 @@
-from .answer_prompt import result_prompt_format
+from .answer_prompt import (cannot_generate_sql_prompt_format,
+                            result_prompt_format)
 from .question_prompt import single_question_prompt_format
 from .sql_prompt import sql_generation_prompt
