0f9d9e4c7354e931f9381c9c68eb8200bcfb5070
sp-v6x <seongsu.park@voyagerx.com>
2024-10-21 22:34:49 +0900

    Fix safety_settings

--- utils/inference_utils.py
+++ utils/inference_utils.py
@@ -15,7 +15,7 @@ def get_model(api_key: str = None):
 def inference(prompt, model, return_json=True):
     from utils.string_utils import parse_json_from_str
 
-    response = model.generate_content(prompt, safety_settings="BLOCK_ONLY_HIGH")
+    response = model.generate_content(prompt, safety_settings="BLOCK_NONE")
 
     if return_json:
         response = parse_json_from_str(response.text)
